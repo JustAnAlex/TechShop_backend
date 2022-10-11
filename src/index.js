f3e670bfec5cc699ee927ed5a8e4d0ae1d980bc1
JustAnAlex <sasha-perchugov2@mail.ru>
2022-10-11 21:33:11 +0300

    'addsomechanges'

--- src/index.js
+++ src/index.js
@@ -15,7 +15,6 @@ const fileupload = require('express-fileupload')
 const router = require('./routes/index')
 const errorHandler = require('./middleware/ErrorHandlingMiddleware')
 
-//const models = require('./models/models')
 const PORT = process.env.PORT || 5000
 
 const  app = express()
