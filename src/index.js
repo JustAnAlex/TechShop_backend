5ed4332782a8876ea683cff262c28c0a69c19898
JustAnAlex <sasha-perchugov2@mail.ru>
2022-10-11 20:14:37 +0300

    'addsomechanges'

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 const path = require('path')
 
-//require('dotenv').config({ path: path.resolve(process.cwd(), 'new.env') })
-require('dotenv').config()
+//
+if (process.env.NODE_ENV === 'production') {
+    require('dotenv').config({ path: path.resolve(process.cwd(), 'prod.env') })
+} else {
+    require('dotenv').config()
+}
+
 
 const express = require('express')
 const sequelize = require('./db')
